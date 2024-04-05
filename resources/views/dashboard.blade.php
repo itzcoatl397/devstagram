005501e0b2b1ff97a928955ad904855e08b0cfd9
itzcoatl397 <itzcoatl3b@gmail.com>
2024-04-04 18:54:00 -0600

    Refactor code to improve performance and readability

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,7 +8,7 @@
     <div class="flex justify-center">
         <div class="w-full md:w-8/12 lg:w-6/12 md:flex ">
             <div class="md:w-8/12 lg:w-6/12 px-5 md:flex ">
-                <img class="" src="{{ asset('img/usuario.svg') }}" alt="Avatar del usuario"/>
+                <img class="" src="{{ asset('img/registrar.jpg') }}" alt="Avatar del usuario"/>
             </div>
             <div class="md:w-8/12 lg:w-6/12 px-5">
                 <p class="text-gray-700 text-2xl">{{ auth()->user()->name }}</p>
--- routes/web.php
+++ routes/web.php
@@ -11,4 +11,4 @@ Route::get('/', function () {
 
 Route::get('/crear-cuenta',[RegisterController::class,'index'])->name('register');
 Route::post('/crear-cuenta',[RegisterController::class,'store'])->name('register');
-Route::get('/muro',[PostController::class,'index'])->name('post.index');
+Route::get('/muro',[PostController::class,'index'])->name('posts.index');
